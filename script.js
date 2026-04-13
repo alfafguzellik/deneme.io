7ce3d77a9c58728836cda6cfba6f8439cd514d42
alfafguzellik <vamos0443@gmail.com>
2026-04-13 13:32:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function whatsappGit(){
 const mesaj = "Merhaba, Alfa Güzellik Merkezi için rezervasyon oluşturmak istiyorum.";
-
 window.open("https://wa.me/905467741041?text=" + encodeURIComponent(mesaj), "_blank");
 }
