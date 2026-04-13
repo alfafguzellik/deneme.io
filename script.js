68fb9b9a5293fca17f34d2af3a3e7829ae643af9
alfafguzellik <vamos0443@gmail.com>
2026-04-13 13:16:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,15 @@
+function showServices(){
+document.getElementById("services").classList.remove("hidden");
+}
+
+function scrollToBooking(){
+document.getElementById("booking").scrollIntoView({behavior:"smooth"});
+}
+
+function openPage(type){
+window.location.href = type + ".html";
+}
+
 function rezervasyonAl(){
-const name = document.getElementById('name').value;
-const service = document.getElementById('service').value;
-const date = document.getElementById('date').value;
-const time = document.getElementById('time').value;
-
-if(!name || !date || !time){
-alert("Lütfen tüm alanları doldurun");
-return;
-}
-
-fetch("https://script.google.com/macros/s/AKfycbzRwYwl9BiE4SLTQ1vT4yx6fh73duU8eEk_zC5zcPZxdfTRNthrp7DuFjjwtjFC5F5gug/exec", {
-method: "POST",
-body: JSON.stringify({
-name: name,
-service: service,
-date: date,
-time: time
-})
-})
-.then(() => {
 alert("Rezervasyon alındı!");
-})
-.catch(() => {
-alert("Hata oluştu!");
-});
-}
-
-function whatsappGonder(){
-window.open("https://wa.me/905000000000", "_blank");
 }
